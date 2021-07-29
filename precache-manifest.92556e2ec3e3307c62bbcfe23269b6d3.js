dedc2de9076f03bd91c34543c63a1a5087fdbcd6
Tommi korhonen <87190300+TommiKorhonen@users.noreply.github.com>
2021-07-29 16:50:22 +0300

    Add files via upload

---
+++ precache-manifest.92556e2ec3e3307c62bbcfe23269b6d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a22fad5a37570e42d5845597e382a78",
+    "url": "/Birthday-react-project/index.html"
+  },
+  {
+    "revision": "55834667fa7b7f1bad0b",
+    "url": "/Birthday-react-project/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/Birthday-react-project/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Birthday-react-project/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55834667fa7b7f1bad0b",
+    "url": "/Birthday-react-project/static/js/main.3ba9b5df.chunk.js"
+  },
+  {
+    "revision": "903fd813fa0b8e99e22e",
+    "url": "/Birthday-react-project/static/js/runtime-main.8303af8d.js"
+  }
+]);
